6b87edddfc5383d3f1bd09eba5426ceb3acb7799
cazmaar <kazeem.alatishe@yahoo.com>
2022-08-15 16:23:00 +0100

    first commit

--- vars/commitgit.groovy
+++ vars/commitgit.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy 
 def call(){
     withCredentials([usernamePassword(credentialsId: 'token-github', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-// sh "git remote set-url origin https://[$token]@github.com/cazmaar/aws_jenkins_drinks_project.git"
-// sh "git add ."
-// sh 'git commit -m "commiting version bump"'
-// sh "git push origin HEAD:main"
-    sh "echo $PASS"
+sh "git remote set-url origin https://$PASS@github.com/cazmaar/aws_jenkins_drinks_project.git"
+sh "git add ."
+sh 'git commit -m "commiting version bump"'
+sh "git push origin HEAD:main"
+    // sh "echo $PASS"
     }
 }
 
